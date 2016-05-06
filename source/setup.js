fc31b5acce0c8eae84237d9de4befa84a1095562
Le Viet Khang <gamekhanglv@gmail.com>
2016-05-06 13:32:48 +0700

    Cap nhat ban 4

--- source/setup.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('myController', function($scope, $http) {
-  $http.get('default.json')
-  .success(function(response) {
-      $scope.myData = response;
-  });
-});
